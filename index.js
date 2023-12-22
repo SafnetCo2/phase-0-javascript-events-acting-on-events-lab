958fbc598f1b260a132f9e8de59ebd37e12fdda7
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-22 17:22:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+/*const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#ff69b4"
+//access the style property of dodger element allows things like background color ,height and width to change
+//move the element up
+dodger.style.bottom = "100px";
+//move to bottom
+dodger.style.bottom = "0px";
+//So what do we mean when we say that an event provides a code?
+/*Any time an event listener is in place and the event it's listening for is triggered, a JavaScript object containing a bunch of information about the event is automatically passed as an argument to the callback function. We can access that object and the information it contains by defining a parameter for the callback. It looks like this*/
+/*dodger.addEventListener("keydown", function (event) {
+    if (event === "ArowLeft") {
+        const left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left - 1}px`;
+        console.log(dodger)
+
+
+    }
+});*/
+//Now we'll build our moveDodgerLeft() function, adding a check on the current position of the dodger:
+
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#ff69b4";
+
+function moveDodgerLeft() {
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+//Now let's wire this up to our event listener:
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+
+    if (left < 200) {
+        dodger.style.left = `${left + 1}px`;
+    }
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowRight") {
+            moveDodgerRight();
+        }
+    });
+}
---
+++ onclick/index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('box');
+btn.addEventListener('click', function (e) {
+    console.log(e)
+
+});
+btn.addEventListener('click', e => {
+    console.log(e)
+});
